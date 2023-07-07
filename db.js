6a8dd891c14a714cc84d3e11f32c69c6b1a8a651
Sourav Sarkar <souravsantipur9635@gmail.com>
2023-07-07 16:39:55 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const connection = mongoose.connect(process.env.mongooseURL);
+const connection = mongoose.connect("mongodb+srv://sourav:sourav@cluster0.10rxc0r.mongodb.net/chatGPT?retryWrites=true&w=majority");
 
 module.exports = {connection}
